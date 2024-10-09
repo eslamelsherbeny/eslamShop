d446c00b09241eb93cdca03b5c99b6529e00a189
eslamm <eslamelshereny16@gmail.com>
2024-10-09 17:12:33 +0300

    Your descriptive commit message

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const createToken = (payload) =>
-  jwt.sign({ userId: payload }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE,
+  jwt.sign({ userId: payload }, `${process.env.JWT_SECRET_Key}`, {
+    expiresIn: `${process.env.JWT_EXPIRE}`,
   });
 
 module.exports = createToken;
